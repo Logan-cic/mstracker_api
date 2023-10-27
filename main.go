fe8279cd8456f562b03e73ee7343e01209674989
logan <logan.cc@outlook.com>
2023-10-27 16:54:47 -0300

    criando dockerfile docker compose e aquivo de configuracao

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"mstracker_api/src/router"
 	"fmt"
 	"log"
-	"mstracker_api/src/router"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("Rodando API...")
-
 	r := router.Gerar()
 
 	log.Fatal(http.ListenAndServe(":5000", r))

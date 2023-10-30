b1112719ef97cdd961f425bfc4b9f027bfe2aff2
logan <logan.cc@outlook.com>
2023-10-30 08:51:16 -0300

    Criando pacote config e .env

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"mstracker_api/src/router"
 	"fmt"
 	"log"
+	"mstracker_api/src/config"
+	"mstracker_api/src/router"
 	"net/http"
 )
 
 func main() {
+	config.Carregar()
+	fmt.Println(config.Porta)
+	
+	fmt.Println(config.StringConexaoBanco)
+
+
 	fmt.Println("Rodando API...")
 	r := router.Gerar()
 
